7c61f1eac1837195eaca5430fa82891d51f25f4c
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2013-08-30 18:51:01 +0200

    Added RestClient stub

--- src/SE/Component/Redmine/Client/Rest/RestClient.php
+++ src/SE/Component/Redmine/Client/Rest/RestClient.php
@@ -10,6 +10,7 @@
 namespace SE\Component\Redmine\Client\Rest;
 
 use \SE\Component\Redmine\Client\ClientInterface;
+use \Guzzle\Http\Client as HttpClient;
 
 /**
  *
@@ -22,10 +23,103 @@ class RestClient implements ClientInterface
 
     /**
      *
+     * @var \Guzzle\Http\Client
+     */
+    protected $httpClient;
+
+    /**
+     *
+     * @var string
+     */
+    protected $baseUrl;
+
+    /**
+     *
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     *
+     * @var array
+     */
+    protected $httpAuth = array();
+
+    /**
+     * @param $httpClient
+     * @param string $baseUrl
+     * @param string $apiKey
+     */
+    public function __construct(HttpClient $httpClient, $baseUrl, $apiKey)
+    {
+        $this->httpClient = $httpClient;
+
+        $this->setApiKey($apiKey);
+        $this->setBaseUrl($baseUrl);
+    }
+
+    /**
+     *
      * @return string
      */
     public function getName()
     {
         return self::CLIENT_NAME;
     }
+
+    /**
+     *
+     * @param string $apiKey
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     *
+     * @param string $baseUrl
+     */
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        return $this->baseUrl;
+    }
+
+    /**
+     *
+     * @param string $httpUser
+     * @param string $httpPass
+     */
+    public function setHttpAuth($httpUser, $httpPass)
+    {
+        $this->httpAuth = array((string)$httpUser, (string)$httpPass);
+    }
+
+    /**
+     *
+     * @return array
+     */
+    public function getHttpAuth()
+    {
+        return $this->httpAuth;
+    }
+
+
 } 
--- tests/SE/Component/Redmine/Tests/Client/Rest/RestClientTest.php
+++ tests/SE/Component/Redmine/Tests/Client/Rest/RestClientTest.php
@@ -23,8 +23,51 @@ class RestClientTest extends \PHPUnit_Framework_TestCase
      */
     public function Can_Be_Instantiated()
     {
-        $client = new \SE\Component\Redmine\Client\Rest\RestClient;
+        $stub = $this->getMock('\Guzzle\Http\Client');
+        $baseUrl = 'http://localhost/redmine';
+        $apiKey = sha1(uniqid(microtime(true), true));
+
+        $client = new \SE\Component\Redmine\Client\Rest\RestClient($stub, $baseUrl, $apiKey);
         $this->assertEquals(\SE\Component\Redmine\Client\Rest\RestClient::CLIENT_NAME, $client->getName());
     }
+
+    /**
+     *
+     * @test
+     */
+    public function Set_Get_ApiKey_BaseUrl()
+    {
+        $stub = $this->getMock('\Guzzle\Http\Client');
+        $baseUrl = 'http://localhost/redmine';
+        $apiKey = sha1(uniqid(microtime(true), true));
+
+        $client = new \SE\Component\Redmine\Client\Rest\RestClient($stub, $baseUrl, $apiKey);
+        $this->assertSame($apiKey, $client->getApiKey());
+        $this->assertSame($baseUrl, $client->getBaseUrl());
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function Set_Get_Http_Auth()
+    {
+        $stub = $this->getMock('\Guzzle\Http\Client');
+        $baseUrl = 'http://localhost/redmine';
+        $apiKey = sha1(uniqid(microtime(true), true));
+        $httpUser = sha1(uniqid(microtime(true), true));
+        $httpPass = sha1(uniqid(microtime(true), true));
+
+        $client = new \SE\Component\Redmine\Client\Rest\RestClient($stub, $baseUrl, $apiKey);
+        $client->setHttpAuth($httpUser, $httpPass);
+
+        $this->assertEquals(array($httpUser, $httpPass), $client->getHttpAuth());
+
+
+
+    }
+
+
+
 
 } 
