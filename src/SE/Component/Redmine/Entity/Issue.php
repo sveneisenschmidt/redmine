f57ae6a4f6bd0e22253f0344f679b8aca4be6097
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2013-09-03 14:03:58 +0200

    possbiel dateTime fix [2]

--- src/SE/Component/Redmine/Entity/Issue.php
+++ src/SE/Component/Redmine/Entity/Issue.php
@@ -186,7 +186,7 @@ class Issue
      *
      * @Serializer\Expose
      * @Serializer\SerializedName("updated_on")
-     * @Serializer\Type("datetime<'Y-m-d\TG:i:sP'>")
+     * @Serializer\Type("datetime<'r'>")
      *
      * @var \DateTime
      */
@@ -197,7 +197,7 @@ class Issue
      *
      * @Serializer\Expose
      * @Serializer\SerializedName("created_on")
-     * @Serializer\Type("datetime<'Y-m-d\TG:i:sP'>")
+     * @Serializer\Type("datetime<'r'>")
      *
      * @var \DateTime
      */
